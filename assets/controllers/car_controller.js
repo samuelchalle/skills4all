f1274c1761bb5a373481a821f6fe5399ebc87263
Samuel Challe <s.challe@sdvi.fr>
2023-07-13 16:22:45 +0200

    weather wip

--- assets/controllers/car_controller.js
+++ assets/controllers/car_controller.js
@@ -1,16 +1,26 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+    static targets = [
+        "temperature",
+    ];
 
     connect() {
+        let temperature = this.temperatureTarget;
+
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
-                var latitude = position.coords.latitude;
-                var longitude = position.coords.longitude;
+                let latitude = position.coords.latitude;
+                let longitude = position.coords.longitude;
 
-                // Envoyer les coordonnées au serveur via AJAX ou un formulaire
-                console.log(latitude);
-                console.log(longitude);
+                // Envoyer les coordonnées à l’api météo
+
+
+                // Récupérer la température depuis le serveur
+
+
+                // Afficher la température dans la page
+                temperature.innerHTML = "22°C";
             });
         } else {
             // Gestion de l’absence de prise en charge de la géolocalisation par le navigateur
